530d4cd1b180ed8fa9320183a8da1928c9cb4f74
Roslan Saidi <roslansaidi@ymail.com>
2025-06-28 12:07:04 +0800

    Update conversion.asm

--- conversion.asm
+++ conversion.asm
@@ -1,75 +1,58 @@
-; SECR/SCSR2033 - COMPUTER ORGANIZATION & ARCHITECTURE
-; PROJECT TITLE - CONVERSION BETWEEN INFORMATION SYSTEMS
-; GROUP - 4
-
-INCLUDE Irvine32.inc  ; Include Irvine32 library for input/output functions
+INCLUDE Irvine32.inc
 
 .data
-; Menu and prompt messages
 menuTitle BYTE ">>> Please select the conversion type:", 0
 menu1 BYTE "1. Binary to Decimal", 0
 menu2 BYTE "2. Decimal to Binary", 0
 menu3 BYTE "3. Binary to Hex", 0
-menu4 BYTE "4. Binary to BCD", 0
+menu4 BYTE "4. Decimal to Hex", 0
 menu5 BYTE "5. Exit", 0
 menuLine BYTE "--------------------------------------------------", 0
-askChoice BYTE "\nEnter your choice: ", 0
+askChoice BYTE "Enter your choice: ", 0
 askBinary BYTE "Please Enter 8-bit binary digits (e.g., 11110000): ", 0
 askDecimal BYTE "Please Enter a decimal integer less than 256: ", 0
 askHex BYTE "Please Enter 8-bit binary digits for HEX conversion: ", 0
-askBCD BYTE "Please Enter 8-bit binary digits for BCD conversion: ", 0
+askDecToHex BYTE "Please Enter a decimal integer less than 256 for HEX conversion: ", 0
+
 byeMsg BYTE "Bye.", 0
 
-; Output labels
 outputLabel1 BYTE "The decimal integer of ", 0
 outputLabel2 BYTE "The binary of ", 0
 outputLabel3 BYTE "The hexadecimal of ", 0
-outputLabel4 BYTE "The BCD of ", 0
-suffixB BYTE "b is ", 0
+outputLabel4 BYTE "The hexadecimal of ", 0
 suffixD BYTE "d", 0
+suffixB BYTE "b", 0
 suffixH BYTE "h", 0
+isLabel BYTE " is ", 0
 
-; Buffers and variables
-binInput BYTE 9 DUP(0)     ; Buffer to store binary input string
-resultStr BYTE 16 DUP(0)   ; Buffer to store binary output string
-number DWORD ?             ; Variable to store numeric result
+invalidInput BYTE "Invalid input. Please enter a number between 1 and 5.", 0
+
+binInput BYTE 16 DUP(0)
+resultStr BYTE 16 DUP(0)
+number DWORD ?
 
 .code
 main PROC
+    call DisplayMenu
 
 menu:
-    call Clrscr                    ; Clear the screen
-    mov edx, OFFSET menuLine       ; Display menu border
+    mov edx, OFFSET askChoice
     call WriteString
-    call CrLf
-    mov edx, OFFSET menuTitle      ; Display menu title
-    call WriteString
-    call CrLf
-    mov edx, OFFSET menu1          ; Display option 1
-    call WriteString
-    call CrLf
-    mov edx, OFFSET menu2          ; Display option 2
-    call WriteString
-    call CrLf
-    mov edx, OFFSET menu3          ; Display option 3
-    call WriteString
-    call CrLf
-    mov edx, OFFSET menu4          ; Display option 4
-    call WriteString
-    call CrLf
-    mov edx, OFFSET menu5          ; Display option 5
-    call WriteString
-    call CrLf
-    mov edx, OFFSET menuLine       ; Display menu border again
-    call WriteString
-    call CrLf
+    call ReadInt
+    cmp eax, 1
+    jl invalidMenu
+    cmp eax, 5
+    jg invalidMenu
+    jmp processChoice
 
-    mov edx, OFFSET askChoice      ; Ask user for menu choice
+invalidMenu:
+    mov edx, OFFSET invalidInput
     call WriteString
-    call ReadInt                   ; Read user input as integer
-    mov ecx, eax                   ; Store choice in ECX
+    call CrLf
+    jmp menu
 
-    ; Branch to selected function
+processChoice:
+    mov ecx, eax
     cmp ecx, 1
     je BinaryToDecimal
     cmp ecx, 2
@@ -77,99 +60,141 @@ menu:
     cmp ecx, 3
     je BinaryToHex
     cmp ecx, 4
-    je BinaryToBCD
+    je DecimalToHex
     cmp ecx, 5
     je ExitProgram
-    jmp menu                       ; If invalid, show menu again
+    jmp menu
 
+DisplayMenu PROC
+    call CrLf
+    mov edx, OFFSET menuTitle
+    call WriteString
+    call CrLf
+    mov edx, OFFSET menu1
+    call WriteString
+    call CrLf
+    mov edx, OFFSET menu2
+    call WriteString
+    call CrLf
+    mov edx, OFFSET menu3
+    call WriteString
+    call CrLf
+    mov edx, OFFSET menu4
+    call WriteString
+    call CrLf
+    mov edx, OFFSET menu5
+    call WriteString
+    call CrLf
+    mov edx, OFFSET menuLine
+    call WriteString
+    call CrLf
+    ret
+DisplayMenu ENDP
 
-; Function 1: Binary to Decimal
-
+; ====== Binary to Decimal ======
 BinaryToDecimal:
     call CrLf
-    mov edx, OFFSET askBinary      ; Prompt for binary input
+    mov edx, OFFSET askBinary
     call WriteString
     mov edx, OFFSET binInput
-    call ReadString                ; Read binary string
+    mov ecx, SIZEOF binInput
+    call ReadString
+    cmp eax, 8
+    jne BinaryToDecimal
+    mov binInput[eax], 0
 
-    xor eax, eax                   ; Clear EAX for result
-    mov esi, OFFSET binInput       ; Point to input string
+    xor eax, eax
+    mov esi, OFFSET binInput
 bitLoop1:
-    mov bl, [esi]                  ; Read character
+    mov bl, [esi]
     cmp bl, 0
-    je showDecimal                 ; End of string
-    shl eax, 1                     ; Shift left (multiply by 2)
+    je showDecimal
+    shl eax, 1
     cmp bl, '1'
     jne skipInc1
-    inc eax                        ; Add 1 if bit is '1'
+    inc eax
 skipInc1:
     inc esi
     jmp bitLoop1
 showDecimal:
-    mov number, eax                ; Store result
+    mov number, eax
     call CrLf
-    mov edx, OFFSET outputLabel1   ; Display result label
+    mov edx, OFFSET outputLabel1
     call WriteString
-    mov edx, OFFSET binInput       ; Show input
+    mov edx, OFFSET binInput
     call WriteString
     mov edx, OFFSET suffixB
     call WriteString
-    mov eax, number                ; Show decimal result
+    mov edx, OFFSET isLabel
+    call WriteString
+    mov eax, number
     call WriteDec
     mov edx, OFFSET suffixD
     call WriteString
     call CrLf
-    call WaitMsg
+    call WaitMsg         ; Wait for key press
+    call Clrscr          ; Clear screen after showing results
+    call DisplayMenu
     jmp menu
 
-; Function 2: Decimal to Binary
-
+; ====== Decimal to Binary ======
 DecimalToBinary:
     call CrLf
-    mov edx, OFFSET askDecimal     ; Prompt for decimal input
+    mov edx, OFFSET askDecimal
     call WriteString
-    call ReadInt                   ; Read decimal number
+    call ReadInt
+    cmp eax, 0
+    jl DecimalToBinary
+    cmp eax, 255
+    jg DecimalToBinary
+    mov number, eax
     mov ecx, eax
-    mov number, eax                ; Store input
 
-    mov edi, OFFSET resultStr
-    add edi, 8                     ; Point to end of buffer
-    mov BYTE PTR [edi], 0         ; Null-terminate
+    mov edi, OFFSET resultStr + 8
+    mov BYTE PTR [edi], 0
     dec edi
-    mov eax, ecx
+
 binaryLoop:
-    and eax, 1                     ; Get lowest bit
-    add al, '0'                    ; Convert to ASCII
+    mov eax, ecx
+    and eax, 1
+    add al, '0'
     mov [edi], al
     dec edi
-    shr ecx, 1                     ; Shift right
+    shr ecx, 1
     cmp ecx, 0
     jne binaryLoop
-    inc edi                        ; Point to start of result
+    inc edi
 
     call CrLf
-    mov edx, OFFSET outputLabel2   ; Display result label
+    mov edx, OFFSET outputLabel2
     call WriteString
     mov eax, number
-    call WriteDec                  ; Show decimal input
+    call WriteDec
     mov edx, OFFSET suffixD
+    call WriteString
+    mov edx, OFFSET isLabel
+    call WriteString
+    mov edx, edi
     call WriteString
     mov edx, OFFSET suffixB
     call WriteString
-    mov edx, edi                   ; Show binary result
-    call WriteString
     call CrLf
-    call WaitMsg
+    call WaitMsg         ; Wait for key press
+    call Clrscr          ; Clear screen after showing results
+    call DisplayMenu
     jmp menu
 
-; Function 3: Binary to Hexadecimal
-
+; ====== Binary to Hex ======
 BinaryToHex:
     call CrLf
-    mov edx, OFFSET askHex         ; Prompt for binary input
+    mov edx, OFFSET askHex
     call WriteString
     mov edx, OFFSET binInput
+    mov ecx, SIZEOF binInput
     call ReadString
+    cmp eax, 8
+    jne BinaryToHex
+    mov binInput[eax], 0
 
     xor eax, eax
     mov esi, OFFSET binInput
@@ -185,63 +210,64 @@ skipInc2:
     inc esi
     jmp bitLoop2
 showHex:
+    mov number, eax
     call CrLf
-    mov edx, OFFSET outputLabel3   ; Display result label
+    mov edx, OFFSET outputLabel3
     call WriteString
     mov edx, OFFSET binInput
     call WriteString
     mov edx, OFFSET suffixB
     call WriteString
-    call WriteHex                  ; Show hexadecimal result
+    mov edx, OFFSET isLabel
+    call WriteString
+    mov eax, number
+    call WriteHexB
     mov edx, OFFSET suffixH
     call WriteString
     call CrLf
-    call WaitMsg
+    call WaitMsg         ; Wait for key press
+    call Clrscr          ; Clear screen after showing results
+    call DisplayMenu
     jmp menu
 
-; Function 4: Binary to BCD
+; ====== Decimal to Hex ======
+DecimalToHex:
+    call CrLf
+    mov edx, OFFSET askDecToHex
+    call WriteString
+    call ReadInt
+    cmp eax, 0
+    jl DecimalToHex
+    cmp eax, 255
+    jg DecimalToHex
+    mov number, eax
 
-BinaryToBCD:
     call CrLf
-    mov edx, OFFSET askBCD         ; Prompt for binary input
+    mov edx, OFFSET outputLabel4
     call WriteString
-    mov edx, OFFSET binInput
-    call ReadString
-
-    xor eax, eax
-    mov esi, OFFSET binInput
-bitLoop3:
-    mov bl, [esi]
-    cmp bl, 0
-    je showBCD
-    shl eax, 1
-    cmp bl, '1'
-    jne skipInc3
-    inc eax
-skipInc3:
-    inc esi
-    jmp bitLoop3
-showBCD:
+    mov eax, number
+    call WriteDec
+    mov edx, OFFSET suffixD
+    call WriteString
+    mov edx, OFFSET isLabel
+    call WriteString
+    mov eax, number
+    call WriteHexB
+    mov edx, OFFSET suffixH
+    call WriteString
     call CrLf
-    mov edx, OFFSET outputLabel4   ; Display result label
-    call WriteString
-    mov edx, OFFSET binInput
-    call WriteString
-    mov edx, OFFSET suffixB
-    call WriteString
-    call WriteBCD                  ; Show BCD result
-    call CrLf
-    call WaitMsg
+    call WaitMsg         ; Wait for key press
+    call Clrscr          ; Clear screen after showing results
+    call DisplayMenu
     jmp menu
 
-; Function 5: Exit Program
-
+; ====== Exit Program ======
 ExitProgram:
     call CrLf
-    mov edx, OFFSET byeMsg         ; Display goodbye message
+    mov edx, OFFSET byeMsg
     call WriteString
     call CrLf
-    exit                           ; Terminate program
+    exit
 
 main ENDP
 END main
